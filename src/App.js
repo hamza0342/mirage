c744d5c4b40d025b83eaabac66b4c31ad3a1e100
hamza0342 <63608271+hamza0342@users.noreply.github.com>
2021-01-09 17:18:55 +0500

    Miragejs Demo with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useEffect, useState} from 'react';
+import './App.css'
 function App() {
+
+  const [books, setBooks] = useState([]);
+  useEffect(()=> {
+
+    setInterval(() => {
+      fetch("/api/books")
+      .then(res => res.json())
+      .then(data =>{
+        console.log(data)
+        setBooks(data)
+      })
+    }, 2000);
+  
+  }, [])
+
+  const addBook = ()=> {
+    const title = prompt("Enter Book title");
+    const author = prompt("Enter Book author");
+
+    fetch("/api/add", {
+    method: 'POST',
+    body: JSON.stringify({author, title})
+    
+  } ).then(res=> res.json).then(data => {
+    console.log(data)
+  })
+} 
+
+  
+  if(!books.length)
+    return <h2>Loading!!!</h2>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Books: </h1>
+      <ul>
+        {books.map(book => {
+          return(
+            <li>
+              
+             <h2>Author : </h2> {book.author} <br />
+              <h2>Book name:</h2> {book.title}
+            </li>
+          )
+        })}
+      </ul>
+      <button onClick={addBook} className="btn">Add Book</button>
     </div>
   );
 }
